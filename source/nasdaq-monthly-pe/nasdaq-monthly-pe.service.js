701973fce5a93be2da3ee58a1679f52c4bcf9a51
Gergő Nagygyörgy <csovis@gmail.com>
2022-03-18 22:05:11 +0100

    fix naming service layer

--- source/nasdaq-monthly-pe/nasdaq-monthly-pe.service.js
+++ source/nasdaq-monthly-pe/nasdaq-monthly-pe.service.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 
 'use strict'
 
-const NasdaqMonthlyPeModule = () => {
+const NasdaqMonthlyPeService = () => {
     const loadData = async (apiKey) => await axios.get(
         `https://data.nasdaq.com/api/v3/datasets/MULTPL/SP500_PE_RATIO_MONTH.json?api_key=${apiKey}`,
         {
@@ -21,4 +21,4 @@ const NasdaqMonthlyPeModule = () => {
     };
 };
 
-module.exports = NasdaqMonthlyPeModule;
+module.exports = NasdaqMonthlyPeService;
